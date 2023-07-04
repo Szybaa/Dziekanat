c87c01d70c361388bf72cd36e053d7f5f611e840
Szybaa <94216988+Szybaa@users.noreply.github.com>
2023-07-04 17:30:53 +0200

    Create/delete course

--- Courses.py
+++ Courses.py
@@ -1,5 +1,6 @@
 import psycopg2
 
+# Connect to the PostgreSQL database
 conn = psycopg2.connect(
     host="localhost",
     database="back",
@@ -7,3 +8,41 @@ conn = psycopg2.connect(
     password="123",
     port=5432
 )
+
+def add_course(course_name, course_description, user_id):
+    try:
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "INSERT INTO courses (name, description, id_user) VALUES (%s, %s, %s)",
+            (course_name, course_description, user_id)
+        )
+
+        conn.commit()
+
+        print("Course added successfully!")
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error adding course:", error)
+
+    finally:
+        cursor.close()
+
+def delete_course(course_id):
+    try:
+        cursor = conn.cursor()
+
+        cursor.execute("DELETE FROM courses WHERE id = %s", (course_id,))
+
+        conn.commit()
+
+        print("Course deleted successfully!")
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error deleting course:", error)
+
+    finally:
+        cursor.close()
+
+add_course("test", "test", 1)
+#delete_course(1)
