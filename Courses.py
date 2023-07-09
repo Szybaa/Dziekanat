17719cef763ff82a5f7438d2e8338b4d82c4d859
Szybaa <94216988+Szybaa@users.noreply.github.com>
2023-07-09 14:30:41 +0200

    conn

--- Courses.py
+++ Courses.py
@@ -24,16 +24,82 @@ course_model = api.model('Course', {
 class CourseResource(Resource):
     @api.expect(course_model)
     def post(self):
-        # Rest of the code for adding a course
-        pass
+        course_data = api.payload
 
-    def delete(self):
-        # Rest of the code for deleting a course
-        pass
+        try:
+            # Create a cursor object to interact with the database
+            cursor = conn.cursor()
+
+            # Execute the INSERT statement
+            cursor.execute(
+                "INSERT INTO courses (name, description, id_user) VALUES (%s, %s, %s)",
+                (course_data['name'], course_data['description'], course_data['id_user'])
+            )
+
+            # Commit the transaction
+            conn.commit()
+
+            return {'message': 'Course added successfully'}, 201
+
+        except (Exception, psycopg2.Error) as error:
+            return {'message': 'Error adding course: {}'.format(error)}, 500
+
+        finally:
+            # Close the cursor
+            cursor.close()
+
+    @api.param('course_id', 'Course ID', type='integer', required=True)
+    def delete(self, course_id):
+        try:
+            # Create a cursor object to interact with the database
+            cursor = conn.cursor()
+
+            # Execute the DELETE statement
+            cursor.execute("DELETE FROM courses WHERE id = %s", (course_id,))
+
+            # Commit the transaction
+            conn.commit()
+
+            return {'message': 'Course deleted successfully'}, 200
+
+        except (Exception, psycopg2.Error) as error:
+            return {'message': 'Error deleting course: {}'.format(error)}, 500
+
+        finally:
+            # Close the cursor
+            cursor.close()
 
     def get(self):
-        # Rest of the code for retrieving courses
-        pass
+        try:
+            # Create a cursor object to interact with the database
+            cursor = conn.cursor()
 
-# Add the resource to the API
+            # Execute the SELECT statement to retrieve all courses
+            cursor.execute("SELECT * FROM courses")
+
+            # Fetch all rows from the cursor
+            rows = cursor.fetchall()
+
+            # Transform rows into a list of dictionaries
+            courses = []
+            for row in rows:
+                course = {
+                    'id': row[0],
+                    'name': row[1],
+                    'description': row[2],
+                    'id_user': row[3]
+                }
+                courses.append(course)
+
+            return {'courses': courses}, 200
+
+        except (Exception, psycopg2.Error) as error:
+            return {'message': 'Error retrieving courses: {}'.format(error)}, 500
+
+        finally:
+            # Close the cursor
+            cursor.close()
+
+# Add resource to the API
 api.add_resource(CourseResource, '/add-course', '/delete-course', '/view-course')
+
